684bd789f733ac42f5c1b1a295c06e026c679021
AKL128 <anthonyluu128@gmail.com>
2022-03-07 06:45:36 -0600

    added thread functions

--- js/function.js
+++ js/function.js
@@ -13,3 +13,52 @@ function showIconBar(){
     iconBar.setAttribute("style", "display:block;");
     navigation.classList.add("hide");
 }
+
+var defaultThreads = [
+    {
+        id: 1,
+        title: "Random Thread 1",
+        author: "Anthony",
+        date: Date.now(),
+        content: "Thread content",
+        comments: [
+            {
+                author: "Joe",
+                date: Date.now(),
+                content: "Yo"
+            },
+            {
+                author: "Keenin",
+                date: Date.now(),
+                content: "Hey Bud"
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Random Thread 2",
+        author: "Joe",
+        date: Date.now(),
+        content: "Thread content 2",
+        comments: [
+            {
+                author: "Anthony",
+                date: Date.now(),
+                content: "What's going on?"
+            },
+            {
+                author: "Johnny",
+                date: Date.now(),
+                content: "IDK"
+            }
+        ]
+    }
+]
+
+var threads = defaultThreads
+if (localStorage && localStorage.getItem('threads')) {
+    threads = JSON.parse(localStorage.getItem('threads'));
+} else {
+    threads = defaultThreads;
+    localStorage.setItem('threads', JSON.stringify(defaultThreads));
+}
